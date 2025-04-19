cc716866015cb83f6a6f323f7b767a3d4d03b80f
busking22 <ysc7064@gmail.com>
2025-04-19 19:58:24 +0900

    publish 0.3.2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 // export const HTTPS_API_ORIGIN = "https://frontend-dev.tryvox.co/api/agent/sdk";
-// export const HTTPS_API_ORIGIN = "https://www.tryvox.co/api/agent/sdk";
-export const HTTPS_API_ORIGIN = "http://localhost:3000/api/agent/sdk";
-export const SDK_VERSION = "0.3.1";
+export const HTTPS_API_ORIGIN = "https://www.tryvox.co/api/agent/sdk";
+// export const HTTPS_API_ORIGIN = "http://localhost:3000/api/agent/sdk";
+export const SDK_VERSION = "0.3.2";
